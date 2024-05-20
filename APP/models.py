d4f0654c3e404c1c55bdaa2e8b8e006307f914a6
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-20 09:08:07 -0500

    Create models.py

---
+++ APP/models.py
@@ -0,0 +1,16 @@
+from .db import db
+
+#modelo 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombreproducto = db.Column(db.String(80), nullable=False)
+    precioproducto = db.Column(db.Float, nullable=False)
+    detalleproducto = db.Column(db.String, nullable=False)
+    ivaproducto = db.Column(db.Float, nullable=False)
+    preciototal = db.Column(db.Float, nullable=False)
+    fechaactualizacion = db.Column(db.Date(), nullable=False)
+    fechacreacion = db.Column(db.Date(), nullable=False)
+    urlImagen = db.Column(db.String(80), nullable=False)
+    sistema = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return f'<Product {self.name}>'
